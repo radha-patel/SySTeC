73b8dfe9477be84e8eb4eabf80846a610b5b2be2
Radha Patel <rrpatel@mit.edu>
2023-09-16 14:53:37 -0400

    Add more kernels

---
+++ level_three/ssyrk.jl
@@ -0,0 +1,38 @@
+using Finch
+using BenchmarkTools
+
+A = Fiber!(Dense(SparseList(Element(0.0))), fsprand((10000, 10000), 0.1))
+C = Fiber!(Dense(Dense(Element(0.0))), zeros(10000, 10000))
+
+C = AA^T (A symmetric)
+Optimization: 1x flops, 2x memory bandwidth
+TIME: 88.357 s
+eval(@finch_kernel function ssyrk(C, A) 
+    for l = _, j = _
+        temp = A[j, l]
+        for i = _
+            C[i, j] += temp * A[i, l]
+        end
+    end
+end)
+
+# C = AA^T (A symmetric)
+# Optimization: 2x flops, 2x memory bandwidth
+# TIME: 102.253 s
+# eval(@finch_kernel function ssyrk(C, A) 
+#     for l = _, j = _
+#         temp1 = A[j, l]
+#         for i = _
+#             if i > j
+#                 temp2 = temp1 * A[i, l]
+#                 C[i, j] += temp2
+#                 C[j, i] += temp2
+#             end 
+#             if i == j
+#                 C[i, j] += temp1 * A[i, l]
+#             end
+#         end
+#     end
+# end)
+
+@btime ssyrk($C, $A)
---
+++ level_three/ssyrk_naive.jl
@@ -0,0 +1,15 @@
+using Finch
+using BenchmarkTools
+
+A = Fiber!(Dense(SparseList(Element(0.0))), fsprand((10000, 10000), 0.1))
+C = Fiber!(Dense(Dense(Element(0.0))), zeros(10000, 10000))
+
+# C = AA^T (A symmetric)
+# TIME: 78.731 s
+eval(@finch_kernel function ssyrk_naive(C, A) 
+    for k = _, j = _, i = _
+        C[i, j] += A[i, k] * A[j, k]
+    end
+end)
+
+@btime ssyrk_naive($C, $A)
