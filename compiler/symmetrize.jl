443d582d4d9665c2fc5475450e918f68fb424fb1
Radha Patel <rrpatel@mit.edu>
2024-01-17 19:49:57 -0600

    Clean and modularize code

--- compiler/symmetrize.jl
+++ compiler/symmetrize.jl
@@ -4,17 +4,70 @@ using RewriteTools
 using RewriteTools.Rewriters
 using Combinatorics
 
+import Finch.FinchNotation: and, or
+
+isindex(ex::FinchNode) = ex.kind === index
+order_canonically(idxs) = sort(idxs, by = i->i.val)
+
+iscommutative(::typeof(or)) = true
+iscommutative(::typeof(and)) = true
+iscommutative(::typeof(|)) = true
+iscommutative(::typeof(&)) = true
+iscommutative(::typeof(+)) = true
+iscommutative(::typeof(*)) = true
+iscommutative(::typeof(min)) = true
+iscommutative(::typeof(max)) = true
+iscommutative(alg) = false
+
+function triangularize(idxs)
+    idxs = order_canonically(idxs)
+    conditions = []
+    for i in 1:length(idxs)-1
+        push!(conditions, call(<=, idxs[i], idxs[i+1]))
+    end
+    return length(conditions) > 1 ? call(and, conditions...) : conditions[1]
+end
+
+# TODO: add condition
+function permute_symmetries(ex, idxs)
+    permuted_exs = []
+    for perm in permutations(idxs)
+        ex_2 = Rewrite(Postwalk(@rule ~idx::isindex => begin
+            idx in idxs ? perm[findfirst(i -> i == idx, idxs)] : idx
+        end))(ex)
+        push!(permuted_exs, ex_2)
+    end
+    return sieve(triangularize(idxs), block(permuted_exs...))
+end
+
+function get_permutable_idxs(rhs, issymmetric)
+    permutable = []
+    Postwalk(@rule access(~tn::issymmetric, ~mode, ~idxs...) => push!(permutable, idxs))(rhs)
+    return permutable
+end
+
+function normalize(ex, issymmetric)
+    _normalize = Rewrite(Postwalk(Chain([
+        # Sort indices of symmetric matrices in canonical order (alphabetical)
+        (@rule access(~tn::issymmetric, ~mode, ~idxs...) => access(tn, mode, order_canonically(idxs)...)),
+        # Sort operands in canonical order
+        (@rule call(~op, ~tns...) => call(op, sort(tns, by = tn->hash(tn))...))
+    ])))
+    _normalize(ex)
+end
+
+# TODO: account for partial symmetry
 function find_symmetry(ex, symmetric_tns)
     @capture ex assign(access(~lhs, updater, ~idxs...), ~op, ~rhs)
 
-    permutable_idxs = []
-    # Sort indices of symmetric matrices in canonical order (alphabetical)
-    Postwalk((x) -> if @capture x access(~tn, reader, ~idxs...)
-        if tn.val in symmetric_tns
-            sort!(idxs, by = i -> i.val)
-            push!(permutable_idxs, idxs)
-        end
-    end)(rhs)
+    # Helper methods
+    issymmetric(tn) = tn.val in symmetric_tns
+
+    permutable_idxs = get_permutable_idxs(rhs, issymmetric)
+    # TODO: assumption that there is one symmetric matrix
+    permuted = permute_symmetries(ex, permutable_idxs[1])
+    normalized = normalize(permuted, issymmetric)
+    display(normalized)
 
     permuted_exs = []
     map = Dict{Vector{FinchNode}, Set{FinchNode}}([])
