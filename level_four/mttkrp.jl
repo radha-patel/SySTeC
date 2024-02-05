4d0b56a441223c8443d691b07d968a90626633f5
Radha Patel <rrpatel@mit.edu>
2024-02-05 13:28:28 -0500

    More versions of MTTKRP

--- level_four/mttkrp.jl
+++ level_four/mttkrp.jl
@@ -8,20 +8,24 @@ n = 100
     symA = [triA[sort([i, j, k])...] for i = 1:n, j = 1:n, k = 1:n]
     # clear diagonals
     for k=1:n, j=1:n, i=1:n 
-        # if i == j || j == k || i == k
+        if i == j || j == k || i == k
         # if j == k
         # if i == j == k
-        #     symA[i, j, k] = 0
-        # end
+            symA[i, j, k] = 0
+        end
     end
 
     C = Tensor(Dense(Dense(Element(0))), zeros(n, n))
     T = Tensor(Dense(Dense(Element(0))), zeros(n, n))
     # _C = Tensor(Dense(Dense(Dense(Dense(Element(0))))), zeros(n, n, n, n))
     # _T = Tensor(Dense(Dense(Dense(Dense(Element(0))))), zeros(n, n, n, n))
-    A = Tensor(Dense(Dense(Dense(Element(0)))), symA)
+    A = Tensor(Dense(SparseList(SparseList(Element(0)))), symA)
     B = Tensor(Dense(Dense(Element(0))), rand(Int, n, n))
     # B = Tensor(Dense(Dense(Element(0))), fill(1, (n, n)))
+
+    # temporaries
+    C_jk = Scalar(0)
+    C_jl = Scalar(0)
 
     eval(@finch_kernel mode=fastfinch function mttkrp_ref(C, A, B)
         C .= 0
@@ -37,24 +41,79 @@ n = 100
     #     end
     # end)
 
-    # eval(@finch_kernel mode=fastfinch function mttkrp_opt1(C, A, B)
-    #     C .= 0
-    #     for j=_, l=_, k=_, i=_
-    #         if k <= l # can replace <= with < if A[i, k, l] = 0 when i == j || j == k || i == k
-    #             let a = A[i, k, l]
-    #                 if i < k
-    #                     C[i, j] += a * B[l, j] * B[k, j]
-    #                 end
-    #                 if i <= k # here too
-    #                     C[k, j] += a * B[l, j] * B[i, j]
-    #                     if k < l
-    #                         C[l, j] += a * B[k, j] * B[i, j]
-    #                     end
-    #                 end
-    #             end
-    #         end
-    #     end
-    # end)
+    # assumes diagonals have been zeroed (~3.8x faster than ref)
+    eval(@finch_kernel mode=fastfinch function mttkrp_opt1(C, A, B)
+        C .= 0
+        for l=_, k=_, i=_, j=_
+            if i < k && k < l
+                let a = A[i, k, l], B_lj = B[j, l], B_kj = B[j, k], B_ij = B[j, i]
+                    C[j, i] += a * B_lj * B_kj
+                    C[j, k] += a * B_lj * B_ij
+                    C[j, l] += a * B_kj * B_ij
+                end
+            end
+        end
+    end)
+
+    # assumes diagonals have been zeroed (~2.6x faster than ref)
+    eval(@finch_kernel mode=fastfinch function mttkrp_opt1_1(C, C_jk, C_jl, A, B)
+        C .= 0
+        for l=_, k=_, j=_
+            C_jk .= 0
+            C_jl .= 0
+            for i=_
+                if i < k && k < l
+                    let a = A[i, k, l], B_lj = B[j, l], B_kj = B[j, k], B_ij = B[j, i]
+                        C[j, i] += a * B_lj * B_kj
+                        C_jk[] += a * B_lj * B_ij
+                        C_jl[] += a * B_kj * B_ij
+                    end
+                end
+            end
+            C[j, k] += C_jk[]
+            C[j, l] += C_jl[]
+        end
+    end)
+
+    # assumes diagonals have been zeroed (~2.2x faster than ref)
+    eval(@finch_kernel mode=fastfinch function mttkrp_opt1_2(C, C_jk, C_jl, A, B)
+        C .= 0
+        for j=_, l=_, k=_
+            C_jk .= 0
+            C_jl .= 0
+            for i=_
+                if i < k && k < l
+                    let a = A[i, k, l], B_lj = B[l, j], B_kj = B[k, j], B_ij = B[i, j]
+                        C[i, j] += a * B_lj * B_kj
+                        C_jk[] += a * B_lj * B_ij
+                        C_jl[] += a * B_kj * B_ij
+                    end
+                end
+            end
+            C[k, j] += C_jk[]
+            C[l, j] += C_jl[]
+        end
+    end)
+
+    # assumes diagonals have been zeroed (~2.8x faster than ref)
+    eval(@finch_kernel mode=fastfinch function mttkrp_opt1_3(C, C_jk, C_jl, A, B)
+        C .= 0
+        for l=_, j=_, k=_
+            C_jk .= 0
+            C_jl .= 0
+            for i=_
+                if i < k && k < l
+                    let a = A[i, k, l], B_lj = B[l, j], B_kj = B[k, j], B_ij = B[i, j]
+                        C[i, j] += a * B_lj * B_kj
+                        C_jk[] += a * B_lj * B_ij
+                        C_jl[] += a * B_kj * B_ij
+                    end
+                end
+            end
+            C[k, j] += C_jk[]
+            C[l, j] += C_jl[]
+        end
+    end)
 
     # eval(@finch_kernel mode=fastfinch function mttkrp_opt2(_C, _T, A, B)
     #     _C .= 0
@@ -89,97 +148,111 @@ n = 100
     #     end
     # end)
 
-    println("evaluating opt3")
-    eval(@finch_kernel mode=fastfinch function mttkrp_opt3(C, A, B)
-        C .= 0
-        for l=_, k=_, i=_, j=_
-            let A_ikl = A[i, k, l], B_kj = B[j, k], B_lj = B[j, l], B_ij = B[j, i]
-                let ik_equal = (i == k), kl_equal = (k == l)
-                let any_equal = ik_equal || kl_equal, all_equal = ik_equal && kl_equal
-                    if i <= k && k <= l
-                        let C_ij = B_kj * A_ikl * B_lj, C_kj = B_ij * A_ikl * B_lj, C_lj = B_kj * B_ij * A_ikl
-                            if !any_equal
-                                C[j, i] += 2 * C_ij
-                                C[j, k] += 2 * C_kj
-                                C[j, l] += 2 * C_lj
-                            end
-                            if any_equal && !all_equal
-                                C[j, i] += C_ij
-                                C[j, k] += C_kj
-                                C[j, l] += C_lj
-                            end
-                            if all_equal
-                                C[j, i] += C_ij
-                            end
-                        end
-                    end
-                end
-                end
-            end
-        end
-    end)
-    println("done evaluating opt3")
-
-    # eval(@finch_kernel mode=fastfinch function mttkrp_opt4(C, A, B)
+    # println("evaluating opt3")
+    # eval(@finch_kernel mode=fastfinch function mttkrp_opt3(C, A, B)
     #     C .= 0
-    #     for j=_, l=_, k=_, i=_
-    #         if i <= k && k <= l 
-    #             C[i, j] += B[k, j] * A[i, k, l] * B[l, j]
-    #             if i < k && k < l 
-    #                 C[i, j] += B[k, j] * A[i, k, l] * B[l, j]
+    #     for l=_, k=_, i=_, j=_
+    #         let A_ikl = A[i, k, l], B_kj = B[j, k], B_lj = B[j, l], B_ij = B[j, i]
+    #             let ik_equal = (i == k), kl_equal = (k == l)
+    #             let any_equal = ik_equal || kl_equal, all_equal = ik_equal && kl_equal
+    #                 if i <= k && k <= l
+    #                     let C_ij = B_kj * A_ikl * B_lj, C_kj = B_ij * A_ikl * B_lj, C_lj = B_kj * B_ij * A_ikl
+    #                         if !any_equal
+    #                             C[j, i] += 2 * C_ij
+    #                             C[j, k] += 2 * C_kj
+    #                             C[j, l] += 2 * C_lj
+    #                         end
+    #                         if any_equal && !all_equal
+    #                             C[j, i] += C_ij
+    #                             C[j, k] += C_kj
+    #                             C[j, l] += C_lj
+    #                         end
+    #                         if all_equal
+    #                             C[j, i] += C_ij
+    #                         end
+    #                     end
+    #                 end
     #             end
-    #         end
-    #         if i < k && k <= l 
-    #             C[k, j] += B[i, j] * A[i, k, l] * B[l, j]
-    #             if k < l 
-    #                 C[k, j] += B[i, j] * A[i, k, l] * B[l, j]
-    #             end
-    #         end
-    #         if i <= k && k < l 
-    #             C[l, j] += B[k, j] * B[i, j] * A[i, k, l] 
-    #             if i < k
-    #                 C[l, j] += B[k, j] * B[i, j] * A[i, k, l] 
     #             end
     #         end
     #     end
     # end)
+    # println("done evaluating opt3")
 
-# begin
-#     if and(<(i, k), <(k, l))
-#         begin
-#         C[i, j] <<+>>= *(2, *(B[k, j], A[i, k, l], B[l, j]))
-#         C[k, j] <<+>>= *(2, *(B[i, j], A[i, k, l], B[l, j]))
-#         C[l, j] <<+>>= *(2, *(B[k, j], B[i, j], A[i, k, l]))
-#         end
-#     end
-#     if and(==(i, k), ==(k, l))
-#         begin
-#         C[i, j] <<+>>= *(B[k, j], A[i, k, l], B[l, j])
-#         end
-#     end
-#     if and(!=(i, k), ==(k, l))
-#         begin
-#         C[k, j] <<+>>= *(B[i, j], A[i, k, l], B[l, j])
-#         end
-#     end
-#     if and(==(i, k), !=(k, l))
-#         begin
-#         C[l, j] <<+>>= *(B[k, j], B[i, j], A[i, k, l])
-#         end
-#     end
-# end
+    # println("evaluating opt4")
+    # eval(@finch_kernel mode=fastfinch function mttkrp_opt4(C, A, B)
+    #     C .= 0
+    #     for l=_, k=_, i=_
+    #         let A_ikl = A[i, k, l]
+    #             let ik_equal = (i == k), kl_equal = (k == l)
+    #             let any_equal = ik_equal || kl_equal, all_equal = ik_equal && kl_equal
+    #                 if i <= k && k <= l
+    #                     for j=_
+    #                         let B_kj = B[j, k], B_lj = B[j, l], B_ij = B[j, i]
+    #                             let C_ij = B_kj * A_ikl * B_lj, C_kj = B_ij * A_ikl * B_lj, C_lj = B_kj * B_ij * A_ikl
+    #                                 if !any_equal
+    #                                     C[j, i] += 2 * C_ij
+    #                                     C[j, k] += 2 * C_kj
+    #                                     C[j, l] += 2 * C_lj
+    #                                 end
+    #                                 if any_equal && !all_equal
+    #                                     C[j, i] += C_ij
+    #                                     C[j, k] += C_kj
+    #                                     C[j, l] += C_lj
+    #                                 end
+    #                                 if all_equal
+    #                                     C[j, i] += C_ij
+    #                                 end
+    #                             end
+    #                         end
+    #                     end
+    #                 end
+    #             end
+    #             end
+    #         end
+    #     end
+    # end)
+    # println("done evaluating opt4")
+
 
     ref = Tensor(Dense(Dense((Element(0)))), zeros(n, n))
     # _ref = Tensor(Dense(Dense(Dense(Dense((Element(0)))))), zeros(n, n, n, n))
     @btime mttkrp_ref($ref, $A, $B)
     # @btime _mttkrp_ref($_ref, $A, $B)
 
-    # @btime mttkrp_opt1($C, $A, $B)
-    # check = Scalar(true)
-    # @finch for j=_, i=_
-    #     check[] &= (2 * C[i, j]) == ref[i, j]
-    # end
-    # @info "check" check[]
+    B_T = Tensor(Dense(Dense(Element(0))), zeros(Int, n, n))
+    @finch for j=_, i=_
+        B_T[i, j] = B[j, i]
+    end
+    @btime mttkrp_opt1($C, $A, $B_T)
+    check = Scalar(true)
+    @finch for j=_, i=_
+        check[] &= (2 * C[j, i]) == ref[i, j]
+    end
+    @info "check" check[]
+
+    C_jk = Scalar(0)
+    C_jl = Scalar(0)
+    @btime mttkrp_opt1_1($C, $C_jk, $C_jl, $A, $B_T)
+    check = Scalar(true)
+    @finch for j=_, i=_
+        check[] &= (2 * C[j, i]) == ref[i, j]
+    end
+    @info "check" check[]
+
+    @btime mttkrp_opt1_2($C, $C_jk, $C_jl, $A, $B)
+    check = Scalar(true)
+    @finch for j=_, i=_
+        check[] &= (2 * C[i, j]) == ref[i, j]
+    end
+    @info "check" check[]
+
+    @btime mttkrp_opt1_3($C, $C_jk, $C_jl, $A, $B)
+    check = Scalar(true)
+    @finch for j=_, i=_
+        check[] &= (2 * C[i, j]) == ref[i, j]
+    end
+    @info "check" check[]
 
     # @btime mttkrp_opt2($_C, $_T, $A, $B)
     # check = Scalar(true)
@@ -195,20 +268,16 @@ n = 100
     # end
     # @info "check" check[]
 
-    B_T = Tensor(Dense(Dense(Element(0))), zeros(Int, n, n))
-    @finch for j=_, i=_
-        B_T[i, j] = B[j, i]
-    end
-    @btime mttkrp_opt3($C, $A, $B_T)
-    check = Scalar(true)
-    @finch for j=_, i=_
-        check[] &= C[j, i] == ref[i, j]
-    end
-    @info "check" check[]
-
-    # @btime mttkrp_opt4($C, $A, $B)
+    # @btime mttkrp_opt3($C, $A, $B_T)
     # check = Scalar(true)
     # @finch for j=_, i=_
-    #     check[] &= C[i, j] == ref[i, j]
+    #     check[] &= C[j, i] == ref[i, j]
+    # end
+    # @info "check" check[]
+
+    # @btime mttkrp_opt4($C, $A, $B_T)
+    # check = Scalar(true)
+    # @finch for j=_, i=_
+    #     check[] &= C[j, i] == ref[i, j]
     # end
     # @info "check" check[]
