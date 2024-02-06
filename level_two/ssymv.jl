7e0ecaa35c184a0392a4f140ee0680a6e0113dbc
Radha Patel <rrpatel@mit.edu>
2024-02-06 17:26:29 -0500

    Update SSYMV

--- level_two/ssymv.jl
+++ level_two/ssymv.jl
@@ -1,29 +1,101 @@
 using Finch
 using BenchmarkTools
 
-A = Fiber!(Dense(SparseList(Element(0.0))), fsprand((10000, 10000), 0.1));
-x = Fiber!(Dense(Element(0.0)), rand(10000));
-y = Fiber!(Dense(Element(0.0)), zeros(10000));
-temp2 = Scalar(0.0)
+n = 10000
+A = Tensor(Dense(SparseList(Element(0))), fsprand(Int, n, n, 0.1))
+x = Tensor(Dense(Element(0)), rand(Int, n))
+y = Tensor(Dense(Element(0)), zeros(n))
+temp = Scalar(0)
+
+# A = Fiber!(Dense(SparseList(Element(0.0))), fsprand((n, n), 0.1));
+# x = Fiber!(Dense(Element(0.0)), rand(n));
+# y = Fiber!(Dense(Element(0.0)), zeros(n));
+# temp = Scalar(0.0)
+
+eval(@finch_kernel mode=fastfinch function ssymv_ref(y, A, x) 
+    y .= 0
+    for j = _, i = _
+        y[i] += A[i, j] * x[j]
+    end
+end)
+
+function ssymv_ref_helper(y, A, x) 
+    time = @belapsed ssymv_ref($y, $A, $x)
+    return (;time = time)
+end
 
 # y = Ax (A symmetric)
 # Optimization: 1x flops, 2x memory bandwidth
 # TIME: ~15ms
-eval(@finch_kernel function ssymv(y, A, x, temp2)
+eval(@finch_kernel mode=fastfinch function ssymv_opt1(y, A, x, temp)
+    y .= 0
     for j = _
-        temp1 = x[j]
-        temp2 .= 0
-        for i = _
-            temp3 = A[i, j]
-            if uptrimask[i, j]
-                y[i] += temp1 * temp3
+        let temp1 = x[j]
+            temp .= 0
+            for i = _
+                let temp3 = A[i, j]
+                    if i <= j
+                        y[i] += temp1 * temp3
+                    end
+                    if i < j
+                        temp[] += temp3 * x[i]
+                    end
+                end
             end
-            if uptrimask[i, j - 1]
-                temp2[] += temp3 * x[i]
-            end
+            y[j] += temp[]
         end
-        y[j] += temp2[]
+    end
+    y
+end)
+
+# eval(@finch_kernel mode=fastfinch function ssymv_opt2(y, A, x)
+#     y .= 0
+#     for j=_, i=_
+#         let A_ij = A[i, j], x_j = x[j]
+#             if i != j
+#                 y[i] += A_ij * x_j 
+#                 y[j] += A_ij * x[i]
+#             end
+#             if i == j
+#                 y[i] += A_ij * x_j
+#             end
+#         end
+#     end
+# end)
+
+eval(@finch_kernel mode=fastfinch function ssymv_opt3(y, A, x, temp)
+    y .= 0
+    for j=_
+        let x_j = x[j]
+            temp .= 0
+            for i=_
+                let A_ij = A[i, j]
+                    if i != j
+                        y[i] += A_ij * x_j 
+                        temp[] += A_ij * x[i]
+                    end
+                    if i == j
+                        y[i] += A_ij * x_j
+                    end
+                end
+            end
+            y[j] += temp[]
+        end
     end
 end)
 
-@btime ssymv($y, $A, $x, $temp2)
+function ssymv_opt_helper(y, A, x) 
+    temp = Scalar(0)
+    time = @belapsed ssymv_opt($y, $A, $x, $temp)
+    return (;time = time)
+end
+
+function main()
+    ref = Tensor(Dense(Element(0)), zeros(n))
+    @btime ssymv_ref($ref, $A, $x)
+    @btime ssymv_opt1($y, $A, $x, $temp)
+    # @btime ssymv_opt2($y, $A, $x)
+    @btime ssymv_opt3($y, $A, $x, $temp)
+end
+
+main()
