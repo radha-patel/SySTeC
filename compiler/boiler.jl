310ac7954ce697f180fff58862825859e3df5cc7
Radha Patel <rrpatel@mit.edu>
2024-02-12 12:54:33 -0500

    More clean up

--- compiler/boiler.jl
+++ compiler/boiler.jl
@@ -9,16 +9,16 @@ l = index(:l)
 
 # expressions to test with
 ex = @finch_program C[i] += A[i, j] * B[j]
-symmetrize3(ex, [A])
+symmetrize(ex, [A])
 ex = @finch_program C[] += B[i] * A[i, j] * B[j]
-symmetrize3(ex, [A])
+symmetrize(ex, [A])
 ex = @finch_program C[i, j] += A[i, k] * B[k, j]
-symmetrize3(ex, [A])
+symmetrize(ex, [A])
 ex = @finch_program C[i, j] += A[i, k] * A[k, j]
-symmetrize3(ex, [A])
+symmetrize(ex, [A])
 ex = @finch_program C[i, j, l] += A[k, j, l] * B[k, i]
-symmetrize3(ex, [A])
+symmetrize(ex, [A])
 ex = @finch_program C[i, j] += A[i, k, l] * B[l, j] * B[k, j]
-symmetrize3(ex, [A])
+symmetrize(ex, [A])
 ex = @finch_program C[i, j, l] += A[i, k, j, l] * B[k, i]
-symmetrize3(ex, [A])
+symmetrize(ex, [A])
