4259ffc330e31325f1310b60108d3307f1c026f3
Radha Patel <rrpatel@mit.edu>
2024-05-10 14:55:21 -0400

    Update SSYMV code

--- level_two/ssymv.jl
+++ level_two/ssymv.jl
@@ -2,15 +2,14 @@ using Finch
 using BenchmarkTools
 
 n = 10000
-A = Tensor(Dense(SparseList(Element(0))), fsprand(Int, n, n, 0.1))
+triA = fsprand(Int, n, n, 0.1)
+symA = [triA[sort([i, j])...] for i = 1:n, j = 1:n]
+
+A = Tensor(Dense(SparseList(Element(0))), symA)
 x = Tensor(Dense(Element(0)), rand(Int, n))
 y = Tensor(Dense(Element(0)), zeros(n))
+diag = Tensor(Dense(Element(0)), zeros(n))
 temp = Scalar(0)
-
-# A = Fiber!(Dense(SparseList(Element(0.0))), fsprand((n, n), 0.1));
-# x = Fiber!(Dense(Element(0.0)), rand(n));
-# y = Fiber!(Dense(Element(0.0)), zeros(n));
-# temp = Scalar(0.0)
 
 eval(@finch_kernel mode=:fast function ssymv_ref(y, A, x) 
     y .= 0
@@ -20,26 +19,32 @@ eval(@finch_kernel mode=:fast function ssymv_ref(y, A, x)
     y
 end)
 
-function ssymv_ref_helper(y, A, x) 
-    time = @belapsed ssymv_ref($y, $A, $x)
-    return (;time = time)
-end
+eval(@finch_kernel mode=:fast function ssymv_gen(y, A, x)
+    y .= 0
+    for j=_, i=_
+        let A_ij = A[i, j]
+            if i <= j
+                y[i] += A_ij * x[j]
+            end
+            if i < j
+                y[j] += A_ij * x[i]
+            end
+        end
+    end
+end)
 
-# y = Ax (A symmetric)
-# Optimization: 1x flops, 2x memory bandwidth
-# TIME: ~15ms
 eval(@finch_kernel mode=:fast function ssymv_opt1(y, A, x, temp)
     y .= 0
     for j = _
-        let temp1 = x[j]
+        let x_j = x[j]
             temp .= 0
             for i = _
-                let temp3 = A[i, j]
+                let A_ij = A[i, j]
                     if i <= j
-                        y[i] += temp1 * temp3
+                        y[i] += x_j * A_ij
                     end
                     if i < j
-                        temp[] += temp3 * x[i]
+                        temp[] += A_ij * x[i]
                     end
                 end
             end
@@ -49,57 +54,49 @@ eval(@finch_kernel mode=:fast function ssymv_opt1(y, A, x, temp)
     y
 end)
 
-# eval(@finch_kernel mode=:fast function ssymv_opt2(y, A, x)
-#     y .= 0
-#     for j=_, i=_
-#         let A_ij = A[i, j], x_j = x[j]
-#             if i != j
-#                 y[i] += A_ij * x_j 
-#                 y[j] += A_ij * x[i]
-#             end
-#             if i == j
-#                 y[i] += A_ij * x_j
-#             end
-#         end
-#     end
-# end)
-
-eval(@finch_kernel mode=:fast function ssymv_opt3(y, A, x, temp)
+eval(@finch_kernel mode=:fast function ssymv_opt2(y, A, x, diag, temp)
     y .= 0
-    for j=_
+    for j = _
         let x_j = x[j]
             temp .= 0
-            for i=_
-                if i <= j
-                    let A_ij = A[i, j], y_i = A_ij * x_j 
-                        if i != j
-                            y[i] += y_i
-                            temp[] += A_ij * x[i]
-                        end
-                        # TODO: benchmark this but with following segment w runtime compilation
-                        if i == j
-                            y[i] += y_i
-                        end
-                    end
+            for i = _
+                let A_ij = A[i, j]
+                    y[i] += x_j * A_ij
+                    temp[] += A_ij * x[i]
                 end
             end
-            y[j] += temp[]
+            y[j] += temp[] + diag[j] * x_j
         end
     end
+    y
 end)
-
-function ssymv_opt_helper(y, A, x) 
-    temp = Scalar(0)
-    time = @belapsed ssymv_opt($y, $A, $x, $temp)
-    return (;time = time)
-end
 
 function main()
     ref = Tensor(Dense(Element(0)), zeros(n))
     @btime ssymv_ref($ref, $A, $x)
+
+    @btime ssymv_gen($y, $A, $x)
+    @info "compiler generated code" ref == y
+
     @btime ssymv_opt1($y, $A, $x, $temp)
-    # @btime ssymv_opt2($y, $A, $x)
-    @btime ssymv_opt3($y, $A, $x, $temp)
+    @info "hand-optimized code #1" ref == y
+
+    _A = Tensor(Dense(SparseList(Element(0))))
+    _d = Tensor(Dense(Element(0)))
+    @finch mode=:fast begin
+        _A .= 0
+        _d .= 0
+        for j = _, i = _
+            if i < j
+                _A[i, j] = A[i, j]
+            end
+            if i == j
+                _d[i] = A[i, j]
+            end
+        end
+    end
+    @btime ssymv_opt2($y, $_A, $x, $_d, $temp)
+    @info "hand-optimized code #2" ref == y
 end
 
 main()
