db426377c66530c6e98f2e5ff36c990ccf7ce292
Radha Patel <rrpatel@mit.edu>
2024-05-10 15:00:57 -0400

    Clean up SYPRD code

--- level_two/syprd.jl
+++ level_two/syprd.jl
@@ -3,34 +3,47 @@ using BenchmarkTools
 using SparseArrays
 
 n = 10000
-triA = rand(Int, n, n)
+triA = fsprand(Int, n, n, 0.1)
 symA = [triA[sort([i, j])...] for i = 1:n, j = 1:n]
-x = rand(Int, n)
 
-A = Fiber!(Dense(SparseList(Element(0))), symA)  
-X = Fiber!(Dense(Element(0)), x)
-C = Scalar(0)
-D = Scalar(0)
+A = Tensor(Dense(SparseList(Element(0))), symA)
+x = Tensor(Dense(Element(0)), rand(Int, n))
+y = Scalar(0)
+diag = Scalar(0)
 
-eval(@finch_kernel mode=:fast function syprd_ref(C, A, X)
-    C .= 0
+eval(@finch_kernel mode=:fast function syprd_ref(y, A, x)
+    y .= 0
     for j=_, i=_ 
-        C[] += X[i] * A[i, j] * X[j] 
+        y[] += x[i] * A[i, j] * x[j] 
     end
 end)
 
-eval(@finch_kernel mode=:fast function syprd_opt1(C, D, A, X)
-    C .= 0
-    D .= 0
+eval(@finch_kernel mode=:fast function syprd_gen(y, A, x)
+    y .= 0
+    for j=_, i=_
+        let x_i = x[i], A_ij = A[i, j], x_j = x[j]
+            if i < j
+                y[] += 2 * A_ij * x_i * x_j
+            end
+            if i == j
+                y[] += A_ij * x_i * x_j
+            end
+        end
+    end
+end)
+
+eval(@finch_kernel mode=:fast function syprd_opt1(y, diag, A, x)
+    y .= 0
+    diag .= 0
     for j=_ 
-        let X_j = X[j]
+        let x_j = x[j]
             for i=_ 
-                let a = A[i, j]
+                let A_ij = A[i, j]
                     if i < j 
-                        C[] += X[i] * a * X_j
+                        y[] += x[i] * A_ij * x_j
                     end
                     if i == j
-                        D[] += X[i] * a * X_j
+                        diag[] += x[i] * A_ij * x_j
                     end
                 end
             end
@@ -42,13 +55,14 @@ end)
 
 function main()
     ref = Scalar(0)
-    @btime syprd_ref($ref, $A, $X)
+    @btime syprd_ref($ref, $A, $x)
 
-    C = Scalar(0)
-    D = Scalar(0)
-    @btime syprd_opt1($C, $D, $A, $X)
+    @btime syprd_gen($y, $A, $x)
+    @info "compiler generated code" ref[] == y[]
 
-    @info ref[] == 2 * C[] + D[]
+    d = Scalar(0)
+    @btime syprd_opt1($y, $d, $A, $x)
+    @info "hand-optimized code #1" ref[] == 2 * y[] + d[]
 end
 
 main()
